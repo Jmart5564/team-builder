23b7ef55641cacda7019547c6c3759b86f306e14
Jmart5564 <Jmart5564@gmail.com>
2022-07-11 11:34:17 -0700

    supabase linked

--- services/client.js
+++ services/client.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_URL = 'https://jhnrgujhjafsvkiqvvfv.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpobnJndWpoamFmc3ZraXF2dmZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU4Nzk4MTMsImV4cCI6MTk3MTQ1NTgxM30.PJNEo0e6dtTDThpt91p1QfXgkZiDBIaN8hTnW9GPO3o';
 
 export const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
